50542753d3510038db79f10ee50ee124f075f1b9
Omar Rodriguez <oarozu@gmail.com>
2021-11-15 11:42:59 -0300

    add migration

---
+++ app/models/user.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const bcrypt = require('bcrypt');
+const config = require('../../config').common.hash;
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notEmpty: true
+      }
+    }
+  });
+
+  User.newUser = async data => {
+    const user = User.create({
+      ...data,
+      password: await bcrypt.hashSync(data, Number(config.salt))
+    });
+    return user;
+  };
+
+  return User;
+};
---
+++ migrations/migrations/20211113231757-users.js
@@ -0,0 +1,36 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true,
+        allowNull: false
+      },
+      first_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        allowNull: false
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      }
+    });
+  },
+
+  down: async queryInterface => {
+    await queryInterface.dropTable('users');
+  }
+};
